28fcf11b25efa88f6b0ae051c3a6ac431bbd864a
Nano <nanomata@gmail.com>
2016-07-05 16:08:40 -0600

    cambios

---
+++ main.c
@@ -0,0 +1,11 @@
+void main (){
+
+int res = funcion(4,5,6,4,3,2,1);
+
+}
+
+int funcion(int x1, int x2, int x3, int x4, int x5, int x6, int x7){
+
+	if(x7 > 4){return (x1+x2+x3+x4-x5-x6);}
+	else{return (x1+x2+x3+x4+x5-x6);}
+}
